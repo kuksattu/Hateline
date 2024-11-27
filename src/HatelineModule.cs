e563043d1125e416fd442475fcec703d06c0d0e6
kuksa <kuksattu@gmail.com>
2024-11-27 13:47:10 +0200

    Add suggested changes to module

--- src/HatelineModule.cs
+++ src/HatelineModule.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Xml;
 using Celeste.Mod.Hateline.CelesteNet;
 using FMOD.Studio;
@@ -12,48 +11,63 @@ namespace Celeste.Mod.Hateline;
 public class HatelineModule : EverestModule
 {
     public static HatelineModule Instance { get; private set; }
-    
+
     public override Type SettingsType => typeof(HatelineModuleSettings);
-    
+
     public static HatelineModuleSettings Settings => (HatelineModuleSettings)Instance._Settings;
-    
+
     public override Type SessionType => typeof(HatelineModuleSession);
-    
+
     public static HatelineModuleSession Session => (HatelineModuleSession)Instance._Session;
+
+    public HatelineModule()
+    {
+        Instance = this; 
+#if DEBUG
+        // debug builds use verbose logging
+        Logger.SetLogLevel(nameof(HatelineModule), LogLevel.Verbose);
+#else
+        // release builds use info logging to reduce spam in log files
+        Logger.SetLogLevel(nameof(KelperModule), LogLevel.Info);
+#endif
+    }
     
+    private static readonly EverestModuleMetadata CelesteNetMetadata = new() { Name = "CelesteNet.Client" };
+
     public bool HasForcedHat => Settings.AllowMapChanges && Session?.MapForcedHat != null;
     public bool ShouldShowHat => HasForcedHat || Settings.Enabled;
-    
+    public bool IsCNetLoaded;
+
     public string? VisibleHat => HasForcedHat ? Session.MapForcedHat : Settings.SelectedHat;
     public string? CurrentHat => ShouldShowHat ? VisibleHat : HAT_NONE;
-    
+
     public const string HAT_NONE = "none";
-    
+
     public int CurrentX => HasForcedHat ? Session.mapsetX : Settings.CrownX;
     public int CurrentY => HasForcedHat ? Session.mapsetY : Settings.CrownY;
-    
+
     public static HashSet<string> hats = new();
-    
+
     public Dictionary<string, Dictionary<string, string>> HatAttributes = new();
     
     // Define custom hat attributes here
-    private readonly List<(string, string)> _hatAttributeDefinitions = new()
-    { // The name of the attribute and its default value
-        ("scaling", "true"),
-        ("flip", "true"),
+    private readonly Dictionary<string, string> _hatAttributeDefinitions = new()
+    {
+        // The name of the attribute and its default value
+        { "scaling", "true" }, 
+        { "flip", "true" },
     };
 
-    public HatelineModule() { Instance = this; }
-
-    public override void Load()
+public override void Load()
     {
         typeof(GravityHelperImports).ModInterop();
         On.Celeste.Player.Added += HookPlayerAdded;
         On.Celeste.Level.LoadLevel += HookLoadLevel;
         On.Celeste.Player.ResetSprite += HookPlayerResetSprite;
+
+        IsCNetLoaded = Everest.Loader.DependencyLoaded(CelesteNetMetadata);
         
-        if (Everest.Modules.Any(m => m.Metadata.Name == "CelesteNet.Client")) 
-            CelesteNetSupport.Load();
+        if (IsCNetLoaded) CelesteNetSupport.Load();
     }
 
     public override void Unload()
@@ -62,8 +76,7 @@ public class HatelineModule : EverestModule
         On.Celeste.Level.LoadLevel -= HookLoadLevel;
         On.Celeste.Player.ResetSprite -= HookPlayerResetSprite;
 
-        if (Everest.Modules.Any(m => m.Metadata.Name == "CelesteNet.Client")) 
-            CelesteNetSupport.Unload();
+        if (IsCNetLoaded) CelesteNetSupport.Unload();
     }
     
     private static void HookLoadLevel(On.Celeste.Level.orig_LoadLevel orig, Level self, Player.IntroTypes playerintro, bool isfromloader)
@@ -75,22 +88,25 @@ public class HatelineModule : EverestModule
     private static void HookPlayerResetSprite(On.Celeste.Player.orig_ResetSprite orig, Player self, PlayerSpriteMode mode)
     {
         orig(self, mode);
-        self.Get<HatComponent>()?.RemoveSelf();
-        self.Add(new HatComponent(Instance.CurrentHat, Instance.CurrentX, Instance.CurrentY));
+        ResetHat(self);
     }
     
     private static void HookPlayerAdded(On.Celeste.Player.orig_Added orig, Player self, Scene scene)
     {
         orig(self, scene);
-        self.Get<HatComponent>()?.RemoveSelf();
-        self.Add(new HatComponent(Instance.CurrentHat, Instance.CurrentX, Instance.CurrentY));
+        ResetHat(self);
     }
-    
     
     public override void CreateModMenuSection(TextMenu menu, bool inGame, EventInstance snapshot)
     {
         base.CreateModMenuSection(menu, inGame, snapshot);
         HatelineSettingsUI.CreateMenu(menu, inGame);
+    }
+    
+    public static void ResetHat(Player self)
+    {
+        self.Get<HatComponent>()?.RemoveSelf();
+        self.Add(new HatComponent(Instance.CurrentHat, Instance.CurrentX, Instance.CurrentY));
     }
     
     public static void ReloadHat()
@@ -103,22 +119,22 @@ public class HatelineModule : EverestModule
         
         CelesteNetSupport.CNetComponent?.SendPlayerHat();
     }
-
+    
     public static void RegisterHats()
     {
         Instance.HatAttributes.Clear();
         hats.Clear();
-        foreach (KeyValuePair<string, SpriteData> data in GFX.SpriteBank.SpriteData)
+        foreach ((string spriteName, SpriteData spriteData) in GFX.SpriteBank.SpriteData)
         {
-            if (!data.Key.StartsWith("hateline_")) continue;
+            if (!spriteName.StartsWith("hateline_")) continue;
             
             // [9..] removes the hateline_ prefix
-            string hatName = data.Key[9..];
-            XmlNode node = data.Value.Sources[0].XML;
+            string hatName = spriteName[9..];
+            XmlNode node = spriteData.Sources[0].XML;
             Dictionary<string, string> attributesToAdd = new();
             
-            foreach ((string name, string defaultValue) in Instance._hatAttributeDefinitions)
-                attributesToAdd[name] = node.Attributes?[name]?.Value ?? defaultValue;
+            foreach ((string attributeName, string defaultValue) in Instance._hatAttributeDefinitions)
+                attributesToAdd[attributeName] = node.Attributes?[attributeName]?.Value ?? defaultValue;
             
             Instance.HatAttributes[hatName] = attributesToAdd;
             
